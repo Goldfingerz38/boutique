6ffb779110c416f6bb8a439c729a9109db69fa72
Franck CANTALE <franck.cantale@Digital-Grenoble.local>
2022-01-25 16:27:49 +0100

    new files

---
+++ item.php
@@ -0,0 +1,17 @@
+
+
+<?php include('header.php');?>
+<div class="osef" style="height: 800px; position : relative; top: 200px;">
+<?php
+  $name = "sneakers";
+  echo $name;
+  $prix = "149 euros";
+  echo $prix;
+  $nom_photo = "AirJordan12.jpg";
+  echo "<img src=$nom_photo>";   
+?>
+
+</div>
+<?php include('footer.php');?>
+
+
---
+++ simple-catalogue.php
@@ -0,0 +1,3 @@
+<?php
+$products = ["iPhone","iPad", "iMac"];
+?>
